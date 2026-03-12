f454e10f72a22c2c692c7696377e83fa1e48d1e5
Gasser278 <47942704+Gasser278@users.noreply.github.com>
2026-03-12 14:14:50 +0100

    Tick and Sync Block CoCos

---
+++ src/main/java/de/monticore/lang/sd4components/_cocos/SyncPortSendsOnceCoCo.java
@@ -0,0 +1,42 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.sd4components._cocos;
+
+import de.monticore.lang.sd4components.SD4ComponentsMill;
+import de.monticore.lang.sd4components._ast.ASTSDPort;
+import de.monticore.lang.sd4components._ast.ASTSDSyncBlock;
+import de.monticore.lang.sdbasis._ast.ASTSDSendMessage;
+import de.se_rwth.commons.logging.Log;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * CoCo: inside sync blocks outgoing ports must send only once.
+ */
+public class SyncPortSendsOnceCoCo implements SD4ComponentsASTSDSyncBlockCoCo {
+
+  public static final String MESSAGE_ERROR = "0xB500A: "
+    + "Port sends multiple times in sync block";
+
+  @Override
+  public void check(ASTSDSyncBlock node) {
+    List<ASTSDPort> traversed = new ArrayList<>();
+    //get messages
+    for (ASTSDSendMessage message : node.streamSDElements()
+      .filter(SD4ComponentsMill.typeDispatcher()::isSDBasisASTSDSendMessage)
+      .map(SD4ComponentsMill.typeDispatcher()::asSDBasisASTSDSendMessage)
+      .collect(Collectors.toList())) {
+      if (message.isPresentSDSource() && SD4ComponentsMill.typeDispatcher().isSD4ComponentsASTSDPort(message.getSDSource())
+      ) {
+        ASTSDPort sourcePort = SD4ComponentsMill.typeDispatcher().asSD4ComponentsASTSDPort(message.getSDSource());
+        if (!traversed.contains(sourcePort)){
+            traversed.add(sourcePort);
+          }else{
+            Log.error(MESSAGE_ERROR, node.get_SourcePositionStart(), node.get_SourcePositionEnd());
+          }
+        }
+      }
+    }
+
+}
--- src/main/java/de/monticore/lang/sd4components/_cocos/TickCoCo.java
+++ src/main/java/de/monticore/lang/sd4components/_cocos/TickCoCo.java
@@ -1,32 +1,57 @@
 package de.monticore.lang.sd4components._cocos;
 
-import de.monticore.lang.sd4components._ast.ASTSDTick;
-import de.monticore.lang.sd4components._ast.ASTSDSequenceDiagram;
-import de.monticore.lang.sd4components._cocos.SD4ComponentsASTSequenceDiagramCoCo;
+import de.monticore.lang.sd4components._ast.ASTSDPort;
+import de.monticore.lang.sdbasis._ast.ASTSDBody;
+import de.monticore.lang.sdbasis._ast.ASTSDSendMessage;
+import de.monticore.lang.sdbasis._cocos.SDBasisASTSDBodyCoCo;
 import de.se_rwth.commons.logging.Log;
+import de.monticore.lang.sd4components.SD4ComponentsMill;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Ensures that ticks are correctly modeled in a sequence diagram.
+ * Two synchronous messages within the same tick lifetime cannot be sent from the same port.
  */
-public class TickCoCo implements SD4ComponentsASTSequenceDiagramCoCo {
+public class TickCoCo implements SDBasisASTSDBodyCoCo {
 
-  public static final String MESSAGE_ERROR = "0xB5009: "
-    + "Tick is not properly modeled in the sequence diagram.";
+  public static final String MESSAGE_ERROR = "0xB500C: "
+    + "Two sync ports send within a tick lifetime.";
 
   @Override
-  public void check(ASTSDSequenceDiagram node) {
-    node.getSDBody().streamSDElements()
-      .filter(e -> e instanceof ASTSDTick)
-      .forEach(tick -> {
-        if (!isValidTick((ASTSDTick) tick)) {
-          Log.error(MESSAGE_ERROR, tick.get_SourcePositionStart(), tick.get_SourcePositionEnd());
-        }
-      });
-  }
+  public void check(ASTSDBody node) {
 
-  private boolean isValidTick(ASTSDTick tick) {
-    //TASK: Implement tick validation method
-    return true;
-  }
+    for(int i = 0; i < node.getSDElementList().size() - 1; i++) {
+      for(int j = i + 1; j < node.getSDElementList().size(); j++) {
+        if(SD4ComponentsMill.typeDispatcher().isSD4ComponentsASTSDTick(node.getSDElement(i)) && SD4ComponentsMill.typeDispatcher().isSD4ComponentsASTSDTick(node.getSDElement(j))) {
+          if(SD4ComponentsMill.typeDispatcher().isSD4ComponentsASTSDTick(node.getSDElement(i + 1))) break;
+            List<ASTSDSendMessage> messages = new ArrayList<>();
+            int t = i + 1;
+            while(t < j) {
+            if(SD4ComponentsMill.typeDispatcher().isSDBasisASTSDSendMessage(node.getSDElement(t))) {
+              messages.add(SD4ComponentsMill.typeDispatcher().asSDBasisASTSDSendMessage(node.getSDElement(t)));
+            }
+            t++;
+          }
+            // handle messages
+          List<ASTSDPort> traversedP = new ArrayList<>();
+          List<ASTSDSendMessage> traversedM = new ArrayList<>();
+            for (ASTSDSendMessage tr : traversedM) {
+              if(tr.isPresentSDSource() && SD4ComponentsMill.typeDispatcher().isSD4ComponentsASTSDPort(tr.getSDSource())){
+                ASTSDPort source = SD4ComponentsMill.typeDispatcher().asSD4ComponentsASTSDPort(tr.getSDSource());
+                if(source.getPortSymbol().getTiming().getName().equals("sync")){
+                  if(traversedP.contains(source)){
+                    Log.error(MESSAGE_ERROR, node.get_SourcePositionStart(), node.get_SourcePositionEnd());
+                  }else{
+                    traversedP.add(source);
+                  }
+                }
+              }
+            }
+            break;
+          }
+        }
+      }
+    }
+
 }
 
